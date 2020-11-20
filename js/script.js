8998a30db75761a66b4fcab8c3b07fdcc33de712
MaximePicard <maximepicard101@icloud.com>
2020-11-20 08:44:18 -0500

    Premiere Partie

--- js/script.js
+++ js/script.js
@@ -2,4 +2,39 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
 
+    //Le jeu
+
+    class Jeu{    //Pour les fonctions d'objets, lettre majuscules
+
+        constructor(){  //initialise les objets
+
+            console.log("creationDuJeu");
+        }
+    }
+
+    //Le serpent
+
+    class Serpent{
+
+        constructor(){
+
+            console.log("creationDuSerpent");
+        }
+    }
+
+    //La pomme
+
+    class Pomme{
+
+        constructor(){
+
+            console.log("creationDeLaPomme");
+        }
+    }
+
+
+    var unePartie = new Jeu(); //creer une nouvelle partie
+
+
+
 });
