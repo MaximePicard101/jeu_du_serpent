80e833db782ba3460318ca2232d2090b821b8b18
MaximePicard <maximepicard101@icloud.com>
2020-11-20 08:59:12 -0500

    Preparation du canavas

--- js/script.js
+++ js/script.js
@@ -6,9 +6,36 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     class Jeu{    //Pour les fonctions d'objets, lettre majuscules
 
-        constructor(){  //initialise les objets
+        constructor(_idSvg, _idPointage){  //initialise les objets, objets utilisent juste pendant cette methode
 
             console.log("creationDuJeu");
+
+            this.s = Snap(_idSvg);  //Afin de ne pas perdre notre variable on la collectionne ici
+
+            this.sortiPointage = document.querySelector(_idPointage);
+
+            this.grandeurCarre = 20;  //de 20px de large
+            this.grandeurGrille = 15; //15 colonnes
+
+        }
+
+        nouvellePartie(){
+
+            this.affichagePointage(1);
+
+            this.pomme = new Pomme();     //avec this, on ratache l'objet a la fonction jeu
+            this.serpent = new Serpent();
+        }
+
+        finPartie(){
+
+        }
+
+        affichagePointage(_lePointage){
+
+            this.sortiPointage.innerHTML = _lePointage;   //on va placer notre pointage dans le html
+
+
         }
     }
 
@@ -33,8 +60,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
 
 
-    var unePartie = new Jeu(); //creer une nouvelle partie
+    var unePartie = new Jeu("#jeu", "#pointage"); //creer une nouvelle partie, en recuperant les elements dans le index.html
 
+    var btnJouer = document.querySelector("#btnJouer");  //recupere le bouton dans le html
+    btnJouer.addEventListener('click', nouvellePartie);
 
+    function nouvellePartie(){
+        unePartie.nouvellePartie();
+    }
 
 });
